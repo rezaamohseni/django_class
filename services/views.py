4abef3b0c0d85ae34cb106777b2b24cb54812990
reza mohseni <155166979+rezaamohseni@users.noreply.github.com>
2024-05-29 21:25:18 +0330

    all

--- services/views.py
+++ services/views.py
@@ -2,14 +2,31 @@ from django.shortcuts import render
 from .models import SpecialService, Team, Skills , Category , Options , Services
 
 # Create your views here.
+def services(request,category = None):
+    # if request.GET.get('category') is not None:
+    #     all_service =Services.objects.filter(Category__title=request.GET.get('category'))
+    if category:
+        all_service = Services.objects.filter(category__title = category)
+        
+    elif request.GET.get('search') is not None:
+        all_service = Services.objects.filter(content__contains = request.GET.get('search'))
 
-
-def services(request):
-    context = {
-        "services" : Services.objects.filter(status=True),
-        "special_services": SpecialService.objects.filter(status=True)
-    }
+    elif request.GET.get('price') is not None:
+        all_service = Services.objects.filter(price__gte = request.GET.get('price'))
+    else:    
+        all_service = Services.objects.filter(status = True)
+    context = {            
+            "services" : all_service,
+            "special_services": SpecialService.objects.filter(status=True)
+        }
     return render(request, 'services/services.html' , context = context)
+
+
+
+
+
+
+
 
 def services_detail(request):
     return render(request, 'services/service-details.html')
